595ce809420a122f5ba51897dfba661720d0d4e0
Daniel Rahamim <github@v6-networks.com>
2023-02-09 18:18:38 -0800

    wip: fix next field logic

--- src/invenflask/static/next_field.js
+++ src/invenflask/static/next_field.js
@@ -12,16 +12,8 @@ for (const aForm of document.querySelectorAll(`.a-form`)) {
   aForm.addEventListener("submit", formNotifyingListener);
 }
 
-// document.getElementById("the-vanilla-form").addEventListener("keypress", function (event) {
-//   if (event.key === "Enter" && document.querySelector(`#override-enter:checked`) !== null && !event.target.matches(`input[type="submit"]`)) {
-//     event.preventDefault();
-//     const substituteEvent = new KeyboardEvent("keypress", {key: "Tab"});
-//     event.target.dispatchEvent(substituteEvent);
-//   }
-// }, {capture: true});
-
 document.getElementById("inputform").addEventListener("keypress", function (event) {
-  if (event.key === "Enter" && document.querySelector(`#override-enter:checked`) !== null && !event.target.matches(`input[type="submit"]`)) {
+  if (event.key === "Enter" && !event.target.matches(`input[type="submit"]`)) {
     event.preventDefault();
     emulateTab.from(event.target).toNextElement();
   }
